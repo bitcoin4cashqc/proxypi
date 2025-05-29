133b17234bd7e6ae323a03add4d92579d452d32e
SoliditySam <bitcoin4cashqc@hotmail.com>
2025-05-29 14:18:36 -0400

    disabled udp completly for now

--- main.sh
+++ main.sh
@@ -142,9 +142,9 @@ sleep 2
 
 echo "Starting tun2socks-linux-arm64..."
 if [[ -n "$PROXY_USER" && -n "$PROXY_PASS" ]]; then
-    tun2socks-linux-arm64 -device tun://$TUN_IF -proxy socks5://$PROXY_USER:$PROXY_PASS@$PROXY_IP:$PROXY_PORT -interface $INET_IF &
+    tun2socks-linux-arm64 -device tun://$TUN_IF -proxy socks5://$PROXY_USER:$PROXY_PASS@$PROXY_IP:$PROXY_PORT -interface $INET_IF --udp-timeout 0 &
 else
-    tun2socks-linux-arm64 -device tun://$TUN_IF -proxy socks5://$PROXY_IP:$PROXY_PORT -interface $INET_IF &
+    tun2socks-linux-arm64 -device tun://$TUN_IF -proxy socks5://$PROXY_IP:$PROXY_PORT -interface $INET_IF --udp-timeout 0 &
 fi
 TUN2SOCKS_PID=$!
 
